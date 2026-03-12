2c0c58e276483c0c3545d499db1ca45a2dc932de
madsaomi <madsaomi13@gmail.com>
2026-03-12 11:18:21 +0500

    command

--- main.py
+++ main.py
@@ -35,6 +35,43 @@ Help = ReplyKeyboardMarkup(
 async def start(message:Message):
     await message.answer(f"Hello {message.from_user.first_name}!")
 
+# /menu
+@dp.message(Command("menu"))
+async def menu_command(message: Message):
+    await message.answer("📂 This is menu", reply_markup=menu)
+
+# /help
+@dp.message(Command("help"))
+async def help_command(message: Message):
+    await message.answer("🆘 Choose help option")
+
+# /settings
+@dp.message(Command("settings"))
+async def settings_command(message: Message):
+    await message.answer("⚙️ Settings section")
+
+# /contact
+@dp.message(Command("contact"))
+async def contact_command(message: Message):
+    await message.answer("📩 Contact: @your_username")
+
+@dp.message()
+async def buttons(message: Message):
+    text = message.text
+
+    if text == "Menu 🗂":
+        await message.answer("📂 You opened menu")
+
+    elif text == "Settings ⚙️":
+        await message.answer("⚙️ Settings menu")
+
+    elif text == "Help 🆘":
+        await message.answer("🆘 Help section")
+
+    elif text == "Contact 🔎":
+        await message.answer("📩 Contact: @your_username")
+
+@dp.message()
 async def main():
     print('pollingg..')
     await dp.start_polling(bot)
