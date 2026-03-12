7d8e2f75910b4becab28619385187bbe70511953
sagindikovabdulaziz <abdulazizsagindikov@gmail.com>
2026-03-12 10:45:00 +0500

    asddd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import asyncio
 from aiogram import Bot,Dispatcher
-from aiogram.types import Message
+from aiogram.types import Message,ReplyKeyboardMarkup,KeyboardButton
 from aiogram.filters import CommandStart,Command
 import os
 
@@ -10,6 +10,27 @@ load_dotenv()
 Token = os.getenv('TOKEN')
 bot = Bot(token=Token)
 dp = Dispatcher()
+menu = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text='Menu 🗂'),
+            KeyboardButton(text="Settings ⚙️"),
+            KeyboardButton(text="Back 🔙")
+        ]
+    ],
+    resize_keyboard=True
+)
+
+Help = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text='Help 🆘'),
+            KeyboardButton(text="Contact 🔎"),
+            KeyboardButton(text="Back 🔙")
+        ]
+    ],
+    resize_keyboard=True
+)
 @dp.message(CommandStart)
 async def start(message:Message):
     await message.answer(f"Hello {message.from_user.first_name}!")
